a50f8626323e668c7d6bacb8bfb5a3e561156f56
Ibrahim Ethem Gursoy <ieg@bilkon-kontrol.com.tr>
2020-02-11 14:10:13 +0300

    Add app features

--- device_control/DeviceControlWpfApp/DeviceComm.cs
+++ device_control/DeviceControlWpfApp/DeviceComm.cs
@@ -1,5 +1,4 @@
-﻿using Grpc.Net.Client;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
--- device_control/DeviceControlWpfApp/MainWindow.xaml.cs
+++ device_control/DeviceControlWpfApp/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Grpc.Net.Client;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using DevControl;
+using Grpc.Core;
 
 namespace DeviceControlWpfApp
 {
@@ -23,11 +23,18 @@ namespace DeviceControlWpfApp
     public partial class MainWindow : Window
     {
         private DevControlService.DevControlServiceClient client;
-        private GrpcChannel channel;
+        private Channel channel;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            txtBusId.Text = "3";
+            txtDevAddr.Text = "80";
+            txtRegAddr.Text = "80";
+            txtReadLength.Text = "2";
+
+            txtEP.Text = "192.168.2.137:9999";
         }
 
         private void btnLed_Click(object sender, RoutedEventArgs e)
@@ -37,32 +44,42 @@ namespace DeviceControlWpfApp
 
         private void btnReadI2C_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(txtBusId.Text, out _) &&
-                int.TryParse(txtDevAddr.Text, out _) &&
-                int.TryParse(txtRegAddr.Text, out _) &&
-                int.TryParse(txtReadLength.Text, out _))
+            try
             {
-                System.Threading.ThreadPool.QueueUserWorkItem(_ =>
+                if (int.TryParse(txtBusId.Text, out _) &&
+                    int.TryParse(txtDevAddr.Text, out _) &&
+                    int.TryParse(txtRegAddr.Text, out _) &&
+                    int.TryParse(txtReadLength.Text, out _))
                 {
-                    var rsp = client.GetI2C(new RequestGetI2C()
+                    var m = new RequestGetI2C()
                     {
                         BusId = int.Parse(txtBusId.Text),
                         DevAddress = int.Parse(txtDevAddr.Text),
                         RegisterAddress = int.Parse(txtRegAddr.Text),
                         ReadLength = int.Parse(txtReadLength.Text)
-                    });
+                    };
 
-                    Console.WriteLine("Read I2C: " + rsp.ToString());
-                    this.lblI2C.Dispatcher.Invoke((Action)(() =>
+                    new System.Threading.Thread(() =>
                     {
-                        this.lblI2C.Text = ByteArrayToString(rsp.Response.ToByteArray());
-                    }));
-                });
+                        try
+                        {
+                            var rsp = client.GetI2C(m, deadline: DateTime.UtcNow.AddMilliseconds(5000));
+
+                            Console.WriteLine("Read I2C: " + ByteArrayToString(rsp.Response.ToByteArray()));
+                            this.lblI2C.Dispatcher.Invoke((Action)(() =>
+                            {
+                                this.lblI2C.Text = ByteArrayToString(rsp.Response.ToByteArray());
+                            }));
+                        }
+                        catch (Exception err) { Console.WriteLine(err); }
+                    }).Start();
+                }
+                else
+                {
+                    MessageBox.Show("Input Error", $"Check inputs", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
-            else
-            {
-                MessageBox.Show("Input Error", $"Check inputs", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            catch (Exception err) { MessageBox.Show("Input Error", $"{err.Message}", MessageBoxButton.OK, MessageBoxImage.Error); }
         }
 
         public static string ByteArrayToString(byte[] ba)
@@ -78,24 +95,36 @@ namespace DeviceControlWpfApp
             var ep = txtEP.Text;
             if (ep?.Length <= 0)
                 return;
-
-            channel = GrpcChannel.ForAddress(ep);
-            client = new DevControlService.DevControlServiceClient(channel);
+            try
+            {
+                channel = new Channel(ep, ChannelCredentials.Insecure);
+                client = new DevControlService.DevControlServiceClient(channel);
+            }
+            catch (Exception err) { MessageBox.Show("Input Error", $"{err.Message}", MessageBoxButton.OK, MessageBoxImage.Error); }
         }
 
         private void btnReadGPIO_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(txtGPIO.Text, out _))
+            try
             {
-                var rsp = client.GetGPIO(new MGPIO()
+                if (int.TryParse(txtGPIO.Text, out _))
                 {
-                    IoPin = int.Parse(txtGPIO.Text)
-                });
+                    var rsp = client.GetGPIO(new MGPIO()
+                    {
+                        IoPin = int.Parse(txtGPIO.Text)
+                    });
+
+                    this.lblGPIO.Dispatcher.Invoke((Action)(() =>
+                    {
+                        this.lblGPIO.Text = $"{rsp.State}";
+                    }));
+                }
+                else
+                {
+                    MessageBox.Show("Input Error", $"Check inputs", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
-            else
-            {
-                MessageBox.Show("Input Error", $"Check inputs", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            catch (Exception err) { Console.WriteLine(err.Message); }
         }
     }
 }
